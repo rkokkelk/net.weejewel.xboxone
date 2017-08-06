9d10b3a5e5596c964e38388269b4980f8a9c6719
Roy Kokkelkoren <roy.kokkelkoren@gmail.com>
2017-08-06 17:04:29 +0200

    Split device func from driver.js to device.js

---
+++ drivers/xboxone/device.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const Homey = require('homey');
+const XboxOn = require('xbox-on');
+
+const xbox_options = {
+    tries: 5,
+    delay: 1000,
+    waitForCallback: false
+  };
+
+class XboxDevice extends Homey.Device {
+
+  onInit(){
+    // Perform default logging of device
+    this.log('device init');
+    this.log('name:', this.getName());
+    this.log('class:', this.getClass());
+
+		// Register homey default capability onoff
+    this.registerCapabilityListener('onoff', this.onCapabilityOnoff.bind(this))
+	}
+
+	// this method is called when the Device is added
+	onAdded() {
+			this.log('xbox added');
+	}
+
+	// this method is called when the Device is deleted
+	onDeleted() {
+			this.log('device deleted');
+	}
+
+	// this method is called when the Device has requested a state change (turned on or off)
+	onCapabilityOnoff( value, opts, callback ) {
+		let settings = this.getSettings();
+
+		try {
+
+			let xbox = new XboxOn( settings['address'], settings['live_id'] );
+
+			xbox.powerOn( xbox_options, ( err ) => {
+
+				if( err ) return Promise.reject( err );
+
+				// Then, emit a callback ( err, result )
+				callback();
+			});
+
+		} catch( err ) {
+		// or, return a Promise
+			return Promise.reject( err );
+		}
+	}
+}
+
+module.exports = XboxDevice;
--- drivers/xboxone/driver.js
+++ drivers/xboxone/driver.js
@@ -11,11 +11,6 @@ const xbox_options = {
 
 class XboxDriver extends Homey.Driver {
 
-  onInit(){
-		// Register homey default capability onoff
-    this.registerCapabilityListener('onoff', this.onCapabilityOnoff.bind(this))
-	}
-
 	onPair( socket ) {
 
 		socket.on('validate', ( data, callback ) => {
@@ -39,37 +34,6 @@ class XboxDriver extends Homey.Driver {
 		});
 	}
 
-	// this method is called when the Device is added
-	onAdded() {
-			this.log('xbox added');
-	}
-
-	// this method is called when the Device is deleted
-	onDeleted() {
-			this.log('device deleted');
-	}
-
-	// this method is called when the Device has requested a state change (turned on or off)
-	onCapabilityOnoff( value, opts, callback ) {
-		let settings = this.getSettings();
-
-		try {
-
-			let xbox = new XboxOn( settings['address'], settings['live_id'] );
-
-			xbox.powerOn( xbox_options, ( err ) => {
-
-				if( err ) return Promise.reject( err );
-
-				// Then, emit a callback ( err, result )
-				callback();
-			});
-
-		} catch( err ) {
-		// or, return a Promise
-			return Promise.reject( err );
-		}
-	}
 }
 
 module.exports = XboxDriver;
